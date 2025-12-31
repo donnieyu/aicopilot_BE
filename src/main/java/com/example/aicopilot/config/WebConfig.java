e66ad5e4fb15fa7e943120204403b7b0435a2c06
Donnie Yu <dyu@bizflow.com>
2025-12-31 14:02:45 +0900

    config: add localhost to allowedOrigins

--- src/main/java/com/example/aicopilot/config/WebConfig.java
+++ src/main/java/com/example/aicopilot/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://aicopilot-fe.vercel.app") // 본인의 Vercel 도메인 주소 입력
+                .allowedOrigins("https://aicopilot-fe.vercel.app", "http://localhost:5173") // 본인의 Vercel 도메인 주소 입력
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
